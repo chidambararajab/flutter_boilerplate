e8e694094d118b9a32a0cc5bc59c27677ba79bfb
chidambararaja.b <chidambararaja.b@accubits.com>
2023-05-12 00:36:45 +0530

    [Comment Transalation]: widgets

--- lib/widgets/my_button.dart
+++ lib/widgets/my_button.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_deer/res/resources.dart';
 import 'package:flutter_deer/util/theme_utils.dart';
 
-/// 默认字号18，白字蓝底，高度48
+/// The default font size is 18, the white text is on a blue background, and the height is 48
 class MyButton extends StatelessWidget {
-
   const MyButton({
     super.key,
     this.text = '',
@@ -38,38 +37,53 @@ class MyButton extends StatelessWidget {
   Widget build(BuildContext context) {
     final bool isDark = context.isDark;
     return TextButton(
-      onPressed: onPressed,
-      style: ButtonStyle(
-        // 文字颜色
-        foregroundColor: MaterialStateProperty.resolveWith((states) {
-            if (states.contains(MaterialState.disabled)) {
-              return disabledTextColor ?? (isDark ? Colours.dark_text_disabled : Colours.text_disabled);
-            }
-            return textColor ?? (isDark ? Colours.dark_button_text : Colors.white);
-          },
-        ),
-        // 背景颜色
-        backgroundColor: MaterialStateProperty.resolveWith((states) {
-          if (states.contains(MaterialState.disabled)) {
-            return disabledBackgroundColor ?? (isDark ? Colours.dark_button_disabled : Colours.button_disabled);
-          }
-          return backgroundColor ?? (isDark ? Colours.dark_app_main : Colours.app_main);
-        }),
-        // 水波纹
-        overlayColor: MaterialStateProperty.resolveWith((states) {
-          return (textColor ?? (isDark ? Colours.dark_button_text : Colors.white)).withOpacity(0.12);
-        }),
-        // 按钮最小大小
-        minimumSize: (minWidth == null || minHeight == null) ? null : MaterialStateProperty.all<Size>(Size(minWidth!, minHeight!)),
-        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(padding),
-        shape: MaterialStateProperty.all<OutlinedBorder>(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(radius),
+        onPressed: onPressed,
+        style: ButtonStyle(
+          // text color
+          foregroundColor: MaterialStateProperty.resolveWith(
+            (states) {
+              if (states.contains(MaterialState.disabled)) {
+                return disabledTextColor ??
+                    (isDark
+                        ? Colours.dark_text_disabled
+                        : Colours.text_disabled);
+              }
+              return textColor ??
+                  (isDark ? Colours.dark_button_text : Colors.white);
+            },
           ),
+          // background color
+          backgroundColor: MaterialStateProperty.resolveWith((states) {
+            if (states.contains(MaterialState.disabled)) {
+              return disabledBackgroundColor ??
+                  (isDark
+                      ? Colours.dark_button_disabled
+                      : Colours.button_disabled);
+            }
+            return backgroundColor ??
+                (isDark ? Colours.dark_app_main : Colours.app_main);
+          }),
+          // water ripple
+          overlayColor: MaterialStateProperty.resolveWith((states) {
+            return (textColor ??
+                    (isDark ? Colours.dark_button_text : Colors.white))
+                .withOpacity(0.12);
+          }),
+          // Button Min Size
+          minimumSize: (minWidth == null || minHeight == null)
+              ? null
+              : MaterialStateProperty.all<Size>(Size(minWidth!, minHeight!)),
+          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(padding),
+          shape: MaterialStateProperty.all<OutlinedBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(radius),
+            ),
+          ),
+          side: MaterialStateProperty.all<BorderSide>(side),
         ),
-        side: MaterialStateProperty.all<BorderSide>(side),
-      ),
-      child: Text(text, style: TextStyle(fontSize: fontSize),)
-    );
+        child: Text(
+          text,
+          style: TextStyle(fontSize: fontSize),
+        ));
   }
 }
